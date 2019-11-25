27c078a833269b4fc998b8708a8de05d924f4608
cosse <theophile.cosse.etu@univ-lille.fr>
2019-11-25 23:57:31 +0100

    feat : unsaved file from previous commit

--- Questionnaires/src/main/java/fil/coo/AnswerFactory.java
+++ Questionnaires/src/main/java/fil/coo/AnswerFactory.java
@@ -1,5 +1,18 @@
 package fil.coo;
 
 public class AnswerFactory {
+	
+	public final static AnswerFactory FACTORY = new AnswerFactory();
+	
+	private static final char MULTI_ANSWER_SEPARATOR = ';';
+	private static final char MULTI_CHOICE_SEPARATOR = '|';
+	
+	public Answer<?> buildAnswer(String answer){
+		if (NumericalAnswer.answerValidator.accept(answer)) {
+			return new NumericalAnswer(new Integer(answer));
+		} else if (YesNoAnswer.answerValidator.accept(answer)){
+			return new YesNoAnswer(YesNo.valueOf(answer));
+		} else if ()
+	}
 
 }

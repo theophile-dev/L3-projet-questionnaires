c083352b0a7936f7709fe74dc9db9fc2ad3e60ee
Theophile Cosse <theophile.cosse.etu@univ-lille.fr>
2019-12-02 22:03:50 +0100

    Update UIFactory.java

--- Questionnaires/src/main/java/fil/coo/ui/UIFactory.java
+++ Questionnaires/src/main/java/fil/coo/ui/UIFactory.java
@@ -1,27 +1,5 @@
 package fil.coo.ui;
 
-/**
- * 
- * @author theophile
- * 
- * Because java is bad / I haven't find out any other way to do it
- * You need to make every child subclass implement the singleton design pattern like this :
- * 
- * private static CLIFactory factoryInstance = null;	
- * private XXXFactory() {}
- * 
- *Override
- *public static XXXFactory getFactory() {
- *
- *		if (factoryInstance == null) {
- *			factoryInstance = new XXXFactory();
- *		}
- *		return factoryInstance;
- *
- *}
- * 
- *
- */
 public abstract class UIFactory {
 		
 	public abstract MultipleChoiceAnswerUI getMultipleChoiceAnswerUI();
